3c0791a5986baa732fd9b4506ce9dc43299c3176
AKstudios <akram@akstudios.com>
2017-09-17 12:56:13 -0500

    tweaks and comments
    made stuff fit in "1 page width" in Atom

--- dbx.py
+++ dbx.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
 # coding=utf-8
+
+# Developed by AKstudios
+# https://github.com/AKstudios
+# Septermber 17, 2017
 
 from picamera import PiCamera
 from time import sleep
@@ -7,25 +12,29 @@ import dropbox
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
 
-TOKEN = 'fJHKSv0VXNdsfgadfAALOLDQuA35lkC5zzRUHXsA3S8UsadWDXEWTFzrqq0shV_'  # of course this is a fake token. duh. Use your own.
-n=0
+# obviously this is a fake token. duh. Use your own.
+TOKEN = 'fJHKSv0VXNdsfgadfAALOLDQuA35lkC5zzRUHXsA3S8UsadWDXEWTFzrqq0shV_'
+n=0 # set counter for incrementing file name
 
 # Setup Dropbox and upload
 def upload():
     global n    # bring global variable into function
-    LOCALFILE = '/home/pi/timelapse/image%s.jpg' % n
-    BACKUPPATH = '/Apps/AK_rpi3/timelapse/image%s.jpg' % n
+    LOCALFILE = '/home/pi/timelapse/image%s.jpg' % n    # set local file path
+    BACKUPPATH = '/Apps/AK_rpi3/timelapse/image%s.jpg' % n  # set Dropbox path
 
     with open(LOCALFILE, 'rb') as f:
         print("\nUploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
         try:
+            # upload file to Dropbox
             dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
+            print("Uploaded! Das wussup, bro.")
         except ApiError as err:
             # This checks for the specific error where a user doesn't have
             # enough Dropbox space quota to upload this file
             if (err.error.is_path() and
                     err.error.get_path().error.is_insufficient_space()):
-                sys.exit("ERROR: Cannot back up; insufficient space. You probably need more money.")
+                sys.exit("ERROR: Cannot back up; insufficient space."
+                        "You probably need more money.")
             elif err.user_message_text:
                 print(err.user_message_text)
                 sys.exit()
@@ -39,19 +48,18 @@ def capture():
     n+=1 # increment file name
     camera.start_preview()  # start camera
     sleep(5)    # give some delay after camera starts
-    camera.capture('/home/pi/timelapse/image%s.jpg' % n)    # takes an image and appends with incrementing numbers
+    camera.capture('/home/pi/timelapse/image%s.jpg' % n)    # capture image
     camera.stop_preview()   # stop camera
 
 # main program
 if __name__ == '__main__':
     camera = PiCamera() # create camera object
-    camera.resolution = (1600, 1200)    # set image resolution; max resolution for module v2 is 3280 × 2464
+    camera.resolution = (1600, 1200)    # set image resolution
+                                        # max: 3280 × 2464 (for camera v2)
 
     # Check for an access token
     if (len(TOKEN) == 0):
-        sys.exit("ERROR: Naw man, Looks like you didn't add your access token. "
-            "Open up backup-and-restore-example.py in a text editor and "
-            "paste in your token in line 14.")
+        sys.exit("ERROR: Naw man, Looks like you didn't add your access token.")
 
     # Create an instance of a Dropbox class, which can make requests to the API.
     print("Creating a Dropbox object using your token...")
@@ -61,12 +69,11 @@ if __name__ == '__main__':
     try:
         dbx.users_get_current_account()
     except AuthError as err:
-        sys.exit("ERROR: Invalid access token; What are you even typing fam? Try re-generating an "
-            "access token from the app console on the web.")
+        sys.exit("ERROR: Invalid access token; What are you even typing fam?"
+            "Try re-generating an access token from app console on the web.")
 
     # Take timelapse forever
     while True:
         capture()   # take image using rpi camera
         upload()    # upload image to dropbox
-        print("Uploaded! Das wussup, bro.")
-        sleep(3600)    # this will take timelapse at the specified interval in seconds
+        sleep(3600)    # take timelapse at the specified interval in seconds
