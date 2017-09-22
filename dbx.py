a41a90db5ce87f853b4e0215be4270c2a6afdcdb
AKstudios <akram@akstudios.com>
2017-09-22 17:31:57 -0500

    changed file format to be current date-time
    - also, removed sleep - call script as a cron job

--- dbx.py
+++ dbx.py
@@ -3,7 +3,7 @@
 
 # Developed by AKstudios
 # https://github.com/AKstudios
-# Septermber 17, 2017
+# Septermber 22, 2017
 
 from picamera import PiCamera
 from time import sleep
@@ -11,30 +11,28 @@ import sys
 import dropbox
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
+import time
+import datetime
 
-# obviously this is a fake token. duh. Use your own.
 TOKEN = 'fJHKSv0VXNdsfgadfAALOLDQuA35lkC5zzRUHXsA3S8UsadWDXEWTFzrqq0shV_'
-n=0 # set counter for incrementing file name
+dt = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
+LOCALFILE = '/home/pi/timelapse/%s.jpg' % dt
+BACKUPPATH = '/Apps/AK_rpi3/timelapse/%s.jpg' % dt
 
 # Setup Dropbox and upload
 def upload():
-    global n    # bring global variable into function
-    LOCALFILE = '/home/pi/timelapse/image%s.jpg' % n    # set local file path
-    BACKUPPATH = '/Apps/AK_rpi3/timelapse/image%s.jpg' % n  # set Dropbox path
-
+    global LOCALFILE
+    global BACKUPPATH
     with open(LOCALFILE, 'rb') as f:
-        print("\nUploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
+        #print("\nUploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
         try:
-            # upload file to Dropbox
             dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
-            print("Uploaded! Das wussup, bro.")
         except ApiError as err:
             # This checks for the specific error where a user doesn't have
             # enough Dropbox space quota to upload this file
             if (err.error.is_path() and
                     err.error.get_path().error.is_insufficient_space()):
-                sys.exit("ERROR: Cannot back up; insufficient space."
-                        "You probably need more money.")
+                sys.exit("ERROR: Cannot back up; insufficient space. You probably need more money.")
             elif err.user_message_text:
                 print(err.user_message_text)
                 sys.exit()
@@ -42,38 +40,41 @@ def upload():
                 print(err)
                 sys.exit()
 
+
 # Capture an image
 def capture():
-    global n    # bring global variable into function
-    n+=1 # increment file name
+    global dt
     camera.start_preview()  # start camera
     sleep(5)    # give some delay after camera starts
-    camera.capture('/home/pi/timelapse/image%s.jpg' % n)    # capture image
+    camera.capture('/home/pi/timelapse/%s.jpg' % dt)    # takes an image and appends with incrementing numbers
     camera.stop_preview()   # stop camera
+
 
 # main program
 if __name__ == '__main__':
     camera = PiCamera() # create camera object
-    camera.resolution = (1600, 1200)    # set image resolution
-                                        # max: 3280 × 2464 (for camera v2)
+    camera.resolution = (1600, 1200)    # set image resolution; max resolution for module v2 is 3280 × 2464
 
     # Check for an access token
     if (len(TOKEN) == 0):
-        sys.exit("ERROR: Naw man, Looks like you didn't add your access token.")
+        sys.exit("ERROR: Naw man, Looks like you didn't add your access token. "
+            "Open up backup-and-restore-example.py in a text editor and "
+            "paste in your token in line 14.")
 
     # Create an instance of a Dropbox class, which can make requests to the API.
-    print("Creating a Dropbox object using your token...")
+    #print("Creating a Dropbox object using your token...")
     dbx = dropbox.Dropbox(TOKEN)
 
     # Check that the access token is valid
     try:
         dbx.users_get_current_account()
     except AuthError as err:
-        sys.exit("ERROR: Invalid access token; What are you even typing fam?"
-            "Try re-generating an access token from app console on the web.")
+        sys.exit("ERROR: Invalid access token; What are you even typing fam? Try re-generating an "
+            "access token from the app console on the web.")
 
     # Take timelapse forever
-    while True:
-        capture()   # take image using rpi camera
-        upload()    # upload image to dropbox
-        sleep(3600)    # take timelapse at the specified interval in seconds
+    #while True:
+    capture()   # take image using rpi camera
+    upload()    # upload image to dropbox
+    #print("Uploaded! Das wussup, bro.")
+        #sleep(3600)    # this will take timelapse at the specified interval in seconds
